54310143371ded64f30ee926fe82c5e03a0a050f
Mini Smith <minismith742@gmail.com>
2022-09-05 17:19:11 +0100

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'users/show'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'reviews/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
