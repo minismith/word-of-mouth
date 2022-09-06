88de5561e0047e87a7a2819f3eb7e46fa64da48a
emily  harris <emily.harris@simplybusiness.co.uk>
2022-09-06 17:09:39 +0100

    more form updates

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,26 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
     @restaurants = Restaurant.all
+    @emojis = ["Incredible", "Pretty nice", "Meh", "Disappointing", "Gross"]
+    @perfect_for = ["Girls night", "Lads night", "Parents visit", "Date night", "Fancy dinner", "Keep it casual"]
   end
 
-  # def create
-  #   @review = Review.new(review_params)
-  # end
+  def create
+    @review = Review.new(review_params)
+    @restaurants = Restaurant.all
+    @emojis = ["Incredible", "Pretty nice", "Meh", "Disappointing", "Gross"]
+    @perfect_for = ["Girls night", "Lads night", "Parents visit", "Date night", "Fancy dinner", "Keep it casual"]
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:restaurant_id, :title, :content, :emoji, :perfect_for, :photos)
+  end
 
 end
