c534372d58890cafe5a7983392e8bcc4b2dfa25e
emily  harris <emily.harris@simplybusiness.co.uk>
2022-09-06 16:10:08 +0100

    review form part done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,14 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def new
+    @review = Review.new
+    @restaurants = Restaurant.all
+  end
+
+  # def create
+  #   @review = Review.new(review_params)
+  # end
+
 end
