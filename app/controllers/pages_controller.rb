892daa1a2bdc5cec2e8ee4c858a7a38a2ffe5741
Lewis Angus <lewisangus35@gmail.com>
2022-09-06 16:19:36 +0100

    card design update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # @restaurants = Restaurant.all added for testing, remove if you need
   end
 end
