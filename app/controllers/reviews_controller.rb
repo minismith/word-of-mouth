71bedff1cde06ad30f2ed6a412654c94c0938d1f
emily  harris <emily.harris@simplybusiness.co.uk>
2022-09-07 12:12:38 +0100

    reviews fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_user, only: %i[create]
+  # before_action :set_user, only: %i[create]
 
   def index
     @reviews = Review.all
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.user = @user
+    @review.user = current_user
     @restaurants = Restaurant.all
     @perfect_for = ["Girls night", "Lads night", "Parents visit", "Date night", "Fancy dinner", "Keep it casual"]
     if @review.save
@@ -26,10 +26,10 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:restaurant_id, :title, :content, :emoji, :perfect_for, :user_id)
+    params.require(:review).permit(:restaurant_id, :title, :content, :emoji, :perfect_for)
   end
 
-  def set_user
-    @user = User.find(params[:user_id])
-  end
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
 end
