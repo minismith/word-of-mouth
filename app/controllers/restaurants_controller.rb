843ca3d61ed80e12edf96a3f3f530d230ff7de86
Mini Smith <minismith742@gmail.com>
2022-09-07 15:07:01 +0100

    Added map and review cards to restaurant profile

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,5 +12,11 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @reviews = Review.where(restaurant_id: params[:id])
+    @markers = [{
+      lat: @restaurant.latitude,
+      lng: @restaurant.longitude,
+      info_window: render_to_string(partial: "info_window", locals: {restaurant: @restaurant})
+    }]
   end
 end
