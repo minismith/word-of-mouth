21c8a635c2ffcaad5ded3213562cae258c09ca0b
Lewis Angus <lewisangus35@gmail.com>
2022-09-08 15:43:58 +0100

    reviews added to review tab (not dynamic)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @reviews = User.review
+    @reviews = Review.where(user: @user)
+    @restaurant = Restaurant.where(Restaurant: @restaurant)
 
   end
 end
