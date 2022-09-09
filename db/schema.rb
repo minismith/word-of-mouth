2c3117019d1e1fba6a153d50c3160398df371620
Lewis Angus <lewisangus35@gmail.com>
2022-09-09 14:34:34 +0100

    text alignment assignment

--- db/schema.rb
+++ db/schema.rb
@@ -81,8 +81,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_130551) do
     t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.text "opening_hours"
     t.string "website"
+    t.text "opening_hours"
   end
 
   create_table "reviews", force: :cascade do |t|
