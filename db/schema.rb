775aef7ca9b5c2ee83b5d13adb184a9914d1ddcf
Mini Smith <minismith742@gmail.com>
2022-09-12 16:39:23 +0100

    Fixed missing closed div issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_12_132044) do
-
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_115706) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,7 +101,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_115706) do
     t.float "longitude"
     t.text "opening_hours", default: [], array: true
     t.string "website"
-
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -114,7 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_115706) do
     t.string "emoji"
     t.text "content"
     t.string "title"
-    t.text "perfect_for"
+    t.string "perfect_for"
     t.integer "likes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
