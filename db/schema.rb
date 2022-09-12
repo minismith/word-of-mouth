2cfb64e346781d7c5822dffbdd4a0e75dd3383a7
emily  harris <emily.harris@simplybusiness.co.uk>
2022-09-12 15:18:08 +0100

    now add photos to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -81,8 +81,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_100942) do
     t.string "photo", default: "https://res.cloudinary.com/dg6mudunt/image/upload/v1662977333/ali-inay-y3aP9oo9Pjc-unsplash_g7gvan.jpg"
     t.float "latitude"
     t.float "longitude"
-    t.text "opening_hours"
     t.string "website"
+    t.text "opening_hours"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -91,7 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_100942) do
     t.string "emoji"
     t.text "content"
     t.string "title"
-    t.string "perfect_for"
+    t.text "perfect_for"
     t.integer "likes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
