99a28699b944df6b1cdde2e9ab3953135271766f
Mini Smith <minismith742@gmail.com>
2022-09-13 16:33:10 +0100

    google api key thing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -54,8 +54,8 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    key = ENV['GOOGLE_MAPS_API_KEY']
-    id_url = URI("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{@restaurant.name}&inputtype=textquery&locationbias=ipbias&fields=place_id&key=#{key}")
+    # key = ENV['GOOGLE_MAPS_API_KEY']
+    id_url = URI("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{@restaurant.name}&inputtype=textquery&locationbias=ipbias&fields=place_id&key=AIzaSyBUm0Tlt9t8A5HxGpzrYqr48YXDk6T44v4")
     restaurant_serialized = URI.open(id_url).read
     restaurant_basics = JSON.parse(restaurant_serialized)
     place_id = restaurant_basics["candidates"][0]["place_id"]
