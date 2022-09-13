4b8daa39bd6a7d65827521ce186f7d5a0dec26b3
Mini Smith <minismith742@gmail.com>
2022-09-13 23:24:20 +0100

    started on chatroom index

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :likes, only: %i[destroy]
 
   resources :reviews
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: %i[index show] do
     resources :messages, only: :create
   end
 
